05e98b3e4072c3c34763aef6c0255d2f7d630142
AT043 <daffa.assyam.thariq.an21@mhsw.pnj.ac.id>
2023-07-17 02:56:23 +0700

    kelar dah -minor2 dikit

--- program/DonoFurniture.cpp
+++ program/DonoFurniture.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int jumlahItem;
 string mainChoice, gudangChoice;
 
+//fungsi untuk generate kode barang
 int kode() {
     srand((unsigned)time(NULL));
     int random = 1000 + (rand() % 2000);
@@ -12,9 +13,10 @@ int kode() {
     return random;
 }
 
+//fungsi untuk mendapatkan tanggal dengan format YYYYMMDD
 int getCurrentDate() {
     // Get the current time
-    std::time_t now = std::time(nullptr);
+    time_t now = time(nullptr);
 
     // Convert the current time to a struct tm
     std::tm* currentTime = std::localtime(&now);
@@ -40,15 +42,17 @@ struct Barang {
     string tujuan;
 };
 
+//data toko cabang per-wilayah gudang
 struct Cabang {
     string gudangA[10];
     string gudangB[10];
     string gudangC[10];
 };
 
-Cabang cabang = { {"depok", "jakarta", "bandung", "bekasi", "cirebon", "karawang", "lampung"},
+Cabang cabang = { {"depok", "tangerang", "bandung", "bekasi", "cirebon", "karawang"},
                   {"solo", "semarang", "yogyakarta", "purwokerto", "cilacap"},
                   {"surabaya", "malang", "banyuwangi", "bali", "jember"} };
+
 
 void border() {
     char x = '=';
@@ -58,11 +62,12 @@ void border() {
     cout << "\n";
 }
 
+//fungsi untuk menambahkan barang masuk ke gudang
 void tambahBarang(vector<Barang>& gudang) {
     long long kodeMasukBarang;
     string namaBarang;
     long long jumlahBarang;
-   	int tglMskBarang;
+    int tglMskBarang;
     string bahanBarang;
     string asalBarang;
     string tujuanBarang;
@@ -104,12 +109,13 @@ void tambahBarang(vector<Barang>& gudang) {
 void tampilkanDaftarBarang(const vector<Barang>& gudang) {
     map<string, vector<Barang>> groupedBarang;
 
-    // Group the Barang objects based on tujuanBarang
+    // Grouping data Barang berdasarkan tujuanBarang
     for (const Barang& barang : gudang) {
         groupedBarang[barang.tujuan].push_back(barang);
     }
 
     // Iterate over the groupedBarang map and display the grouped Barang objects
+    // Loop
     for (const auto& entry : groupedBarang) {
         const string& tujuanBarang = entry.first;
         const vector<Barang>& barangGroup = entry.second;
@@ -131,7 +137,7 @@ void tampilkanDaftarBarang(const vector<Barang>& gudang) {
     }
 }
 
-
+//fungsi untuk menampilkan 5 data barang paling lama di gudang
 void tampilkanDaftarBarangMasuk(const vector<Barang>& gudang) {
     int index = 0;
     for (const Barang& barang : gudang) {
@@ -155,6 +161,7 @@ void tampilkanDaftarBarangMasuk(const vector<Barang>& gudang) {
     cout << endl;
 }
 
+//fungsi untuk menemukan barang berdasarkan nama barangnya
 void temukanBarang(const vector<Barang>& gudang) {
     string namaBarang;
 
@@ -187,7 +194,7 @@ void temukanBarang(const vector<Barang>& gudang) {
     }
 }
 
-// Fungsi untuk menghapus barang dari gudang
+// Fungsi untuk menghapus barang keluar dari gudang
 void hapusBarang(vector<Barang>& gudang) {
     string namaBarang;
     long long jumlahBarang;
@@ -207,7 +214,7 @@ void hapusBarang(vector<Barang>& gudang) {
             it->jumlah -= jumlahBarang;
             
             
-            // Check if jumlah becomes zero and erase the element
+            // Cek jika jumlahBarang == 0, maka Barang dihapus
             if (it->jumlah == 0) {
                 gudang.erase(it);
                 barangDitemukan = true;
@@ -218,7 +225,7 @@ void hapusBarang(vector<Barang>& gudang) {
     }
 
     if (barangDitemukan) {
-        cout << "Barang sedang dikirim ke tempat tujuan.\n\n";
+        cout << "Barang sedang dikirim ke toko tujuan.\n\n";
     } else {
         cout << "Barang tidak ditemukan di gudang.\n\n";
     }
@@ -232,7 +239,6 @@ void pindahkanBarang(vector<Barang>& gudangAsal, vector<Barang>& gudangTujuan) {
     string namaBarang;
 
     cout << "Masukkan nama barang yang ingin dipindahkan: ";
-    //cin>>namaBarang;
     getline(cin, namaBarang); transform(namaBarang.begin(), namaBarang.end(), namaBarang.begin(), [](unsigned char c) {
         return tolower(c);
     });  
@@ -333,7 +339,7 @@ void menuChoiceHeader() {
         cout << "Pencarian Barang\n";
     }
     else if (mainChoice == "4") {
-        cout << "Info Dono Furniture\n";
+        cout << "Info Gudang dan Toko Dono\n";
     }
     else if(mainChoice == "5"){
     	cout<<"Keluar dari Aplikasi\n";
@@ -369,7 +375,7 @@ int main() {
     bool mainBack;
     do {
         mainHeader();
-        cout << "\nKetik:\n1 Untuk Buka Akses Gudang\n2 Untuk Cek Barang Masuk\n3 Untuk Cari Barang\n4 Untuk Info Gudang\n5 Untuk Keluar\n\n";
+        cout << "\nKetik:\n 1 Untuk Buka Akses Gudang\n 2 Untuk Cek Barang Masuk\n 3 Untuk Cari Barang\n 4 Untuk Info Gudang\n 5 Untuk Keluar\n\n";
         border();
         cin >> mainChoice;
         border();
@@ -378,14 +384,14 @@ int main() {
             bool gudangBack;
             do {
                 menuChoiceHeader();
-                cout << "\nKetik:\n 1 | Untuk Masuk ke Gudang A\n 2 | Untuk Masuk ke Gudang B\n 3 | Untuk Masuk ke Gudang C\n 4 | Untuk Kembali ke Menu Awal\n\n";
+                cout << "\nKetik:\n 1 Untuk Masuk ke Gudang A\n 2 Untuk Masuk ke Gudang B\n 3 Untuk Masuk ke Gudang C\n 4 Untuk Kembali ke Menu Awal\n\n";
                 border();
                 cin >> gudangChoice;
                 system("cls");
                 string pilihanGudangA;
                 if (gudangChoice == "1") {
                     aksesGudangHeader();
-                    cout << "\nKetik:\n 1 | Untuk Cek Barang Gudang A\n 2 | Untuk Tambah Barang Masuk\n 3 | Untuk Tambah Barang Keluar\n 4 | Untuk Kembali ke Menu Akses Gudang\n 5 | Untuk Kembali Ke Main Menu\n\n";
+                    cout << "\nKetik:\n 1 Untuk Cek Barang Gudang A\n 2 Untuk Tambah Barang Masuk\n 3 Untuk Tambah Barang Keluar\n 4 Untuk Kembali ke Menu Akses Gudang\n 5 Untuk Kembali Ke Main Menu\n\n";
                     border();
                     cin >> pilihanGudangA;
                     system("pause");
@@ -396,7 +402,7 @@ int main() {
                         string barangGudangA;
                         cout << "\n";
                         border();
-                        cout << "Ketik:\n 1 | Untuk Cari Barang\n 2 | Untuk Kembali ke Menu Gudang\n";
+                        cout << "Ketik:\n 1 Untuk Cari Barang\n 2 Untuk Kembali ke Menu Gudang\n";
                         border();
                         cin >> barangGudangA;
                         system("cls");
@@ -407,7 +413,7 @@ int main() {
                         		aksesGudangHeader();
                             	temukanBarang(gudangA);
                             	border();
-                            	cout<<"ketik:\n 1 | untuk cari barang lagi\n 2 | untuk kembali ke menu gudang\n";
+                            	cout<<"ketik:\n 1 Untuk cari barang lagi\n 2 Untuk kembali ke menu gudang\n";
                             	cin>>temukanBarangChoice;
                             	if(temukanBarangChoice == "1"){
                             		temukanBarangA = 1;
@@ -444,7 +450,7 @@ int main() {
                             }
                             cout << "\n";
                             border();
-                            cout << "Ketik:\n 1 | Untuk Tambah Barang Lagi/n 2 | Untuk Kembali ke Menu Gudang A\n";
+                            cout << "Ketik:\n 1 Untuk Tambah Barang Lagi\n 2 Untuk Kembali ke Menu Gudang A\n";
                             cin >> tambahGudangA;
                             border();
                             system("pause");
@@ -475,8 +481,7 @@ int main() {
                         cout << "\n";
                         aksesGudangHeader();
                         string tujuanBarang;
-                        cout<<"masukkan tujuan barang: \n";
-                        border();
+                        cout<<"\nmasukkan tujuan barang: \n";
                         cin.ignore();getline(cin, tujuanBarang);transform(tujuanBarang.begin(), tujuanBarang.end(), tujuanBarang.begin(), [](unsigned char c) {
         					return tolower(c);
     					});
@@ -512,7 +517,7 @@ int main() {
 						}
 
 						if (!ketemu && !ketemuB && !ketemuC) {
-    						cout << "Maaf toko/gudang kami belum tersedia di kota tersebut\n";
+    						cout << "Maaf toko/gudang Dono belum tersedia di kota tersebut\n";
 						}
 
 					border();
@@ -544,7 +549,7 @@ int main() {
                     border();
                     cout << "\n";
                     aksesGudangHeader();
-                    cout << "\nKetik:\n 1 | Untuk Cek Barang Gudang B\n 2 | Untuk Tambah Barang Masuk\n 3 | Untuk Tambah Barang Keluar\n 4 | Untuk Kembali ke Menu Gudang\n 5 | Untuk Kembali ke Main Menu\n\n";
+                    cout << "\nKetik:\n 1 Untuk Cek Barang Gudang B\n 2 Untuk Tambah Barang Masuk\n 3 Untuk Tambah Barang Keluar\n 4 Untuk Kembali ke Menu Gudang\n 5 Untuk Kembali ke Main Menu\n\n";
                     border();
                     cin >> pilihanGudangB;
                     system("cls");
@@ -554,7 +559,7 @@ int main() {
                         string barangGudangB;
                         cout << "\n";
                         border();
-                        cout << "\nKetik: 1 | Untuk Cari Barang \n 2 | Untuk Kembali ke Menu Gudang\n";
+                        cout << "\nKetik: 1 Untuk Cari Barang\n 2 Untuk Kembali ke Menu Gudang\n";
                         border();
                         cin >> barangGudangB;
                         system("cls");
@@ -584,7 +589,7 @@ int main() {
                             }
                             cout << "\n";
                             border();
-                            cout << "Ketik\n 1 | Untuk Tambah Barang Lagi\n 2 | Untuk Kembali ke Menu Gudang\n";
+                            cout << "Ketik\n 1 Untuk Tambah Barang Lagi\n 2 Untuk Kembali ke Menu Gudang\n";
                             cin >> tambahGudangB;
                             border();
                             system("pause");
@@ -616,8 +621,7 @@ int main() {
                         cout << "\n";
                         aksesGudangHeader();
                         string tujuanBarang;
-                        cout<<"masukkan tujuan barang: \n";
-                        border();
+                        cout<<"\nmasukkan tujuan barang: \n";
                         cin.ignore();getline(cin, tujuanBarang);transform(tujuanBarang.begin(), tujuanBarang.end(), tujuanBarang.begin(), [](unsigned char c) {
         					return tolower(c);
     					});
@@ -653,7 +657,7 @@ int main() {
 						}
 
 						if (!ketemu && !ketemuA && !ketemuC) {
-    						cout << "Maaf toko/gudang kami belum tersedia di kota tersebut\n";
+    						cout << "Maaf toko/gudang Dono belum tersedia di kota tersebut\n";
 						}
 					border();
 					system("pause");
@@ -684,7 +688,7 @@ int main() {
                     cout << "\n";
                     string pilihanGudangC;
                     aksesGudangHeader();
-                    cout << "\nKetik:\n 1 | Untuk Cek Barang Gudang C\n 2 | Tambah Barang Masuk\n 3 | Tambah Barang Keluar\n 4 | Back to Akses Gudang\n 5 | Back to Main Menu\n\n";
+                    cout << "\nKetik:\n 1 Untuk Cek Barang Gudang C\n 2 Tambah Barang Masuk\n 3 Tambah Barang Keluar\n 4 Back to Akses Gudang\n 5 Back to Main Menu\n\n";
                     border();
                     cin >> pilihanGudangC;
                     system("cls");
@@ -694,7 +698,7 @@ int main() {
                         string barangGudangC;
                         cout << "\n";
                         border();
-                        cout << "\nKetik:\n 1 | Cari Barang \n 2 | Kembali\n";
+                        cout << "\nKetik:\n 1 Cari Barang\n 2 Kembali\n";
                         border();
                         cin >> barangGudangC;
                         system("cls");
@@ -725,7 +729,7 @@ int main() {
                             }
                             cout << "\n";
                             border();
-                            cout << "\nKetik:\n 1 | Tambah Barang Lagi\n 2 | Kembali\n";
+                            cout << "\nKetik:\n 1 Tambah Barang Lagi\n 2 Kembali\n";
                             cin >> tambahGudangC;
                             border();
                             system("pause");
@@ -756,8 +760,7 @@ int main() {
                         cout << "\n";
                         aksesGudangHeader();
                         string tujuanBarang;
-                        cout<<"masukkan tujuan barang: \n";
-                        border();
+                        cout<<"\nmasukkan tujuan barang: \n";
                         cin.ignore();getline(cin, tujuanBarang);transform(tujuanBarang.begin(), tujuanBarang.end(), tujuanBarang.begin(), [](unsigned char c) {
         					return tolower(c);
     					});
@@ -793,7 +796,7 @@ int main() {
 						}
 
 						if (!ketemu && !ketemuA && !ketemuB) {
-    						cout << "Tujuan barang tidak valid\n";
+    						cout << "Maaf toko/gudang Dono belum tersedia di kota tersebut\n";
 						}
 
 					cout << "\n";
@@ -858,7 +861,7 @@ int main() {
         else if (mainChoice == "3") {
             string pilCariGudang;
             menuChoiceHeader();
-            cout << "\nKetik: \n 1 | Untuk Cari Barang di Gudang A\n 2 | Untuk Cari Barang di Gudang B\n 3 | Untuk Cari Barang di Gudang C\n\n";
+            cout << "\nKetik:\n 1 Untuk Cari Barang di Gudang A\n 2 Untuk Cari Barang di Gudang B\n 3 Untuk Cari Barang di Gudang C\n\n";
             border();
             cin >> pilCariGudang;
             system("cls");
@@ -895,22 +898,25 @@ int main() {
         else if (mainChoice == "4") {
         	string infoGudang;
             menuChoiceHeader();
-            cout<<"\n1. Info Dono Furniture\n2. Info Gudang dan cabang\n3. Keluar\n";
+            cout<<"Ketik:\n 1 Untuk melihat info Toko Dono\n 2 Untuk melihat wilayah Gudang dan cabang Toko Dono\n 3 Untuk Kembali ke menu utama\n";
             border();
             cin>>infoGudang;
             system("cls");
             if (infoGudang == "1"){
-            	cout<<"Happy Birthday!\n";
+            	menuChoiceHeader();
+            	cout<<"\nInformasi lebih lanjut hubungi: \n1. 08999999999 (Toko Dono Depok)\n2. 08777777777 (Toko Dono Solo)\n3. 0855555555 (Toko Dono Surabaya)\n email: cs@tokodono.com\n\n";
             	border();
             	system("pause");
             	system("cls");
             	mainBack = true;
 			} else if(infoGudang == "2"){
-				cout<<"\n1. Gudang A-Depok (Depok, Jakarta, Bandung, Bekasi, Karawang, Lampung)\n2. Gudang B-Solo (Solo Raya, Semarang, Purwokerto, Cilacap)\n3. Gudang C-Surabaya (Surabaya, Malang, Banyuwangi, Jember, Bali)\n";
+				menuChoiceHeader();
+				cout<<"\n1. Gudang A-Depok (Depok, Tangerang, Bandung, Bekasi, Karawang)\n2. Gudang B-Solo (Solo Raya, Semarang, Purwokerto, Cilacap)\n3. Gudang C-Surabaya (Surabaya, Malang, Banyuwangi, Jember, Bali)\n";
 				system("pause");
 				system("cls");
 				mainBack = true;
 			} else if(infoGudang == "3"){
+				menuChoiceHeader();
 				cout<<"kembali ke menu utama\n";
 				system("pause");
 				system("cls");
@@ -921,7 +927,7 @@ int main() {
 			menuChoiceHeader();
 			cout<<"\nYakin ingin keluar dari aplikasi?\njika Anda keluar maka seluruh data\nakan ter-reset ulang\n\n";
 			border();
-			cout<<"\nKetik:\n 1 | Untuk Keluar\n 2 | Untuk Kembali ke menu utama\n";
+			cout<<"\nKetik:\n 1 Untuk Keluar\n 2 Untuk Kembali ke menu utama\n";
 			border();
 			cin>>quit;
 			if(quit=="1"){
@@ -950,5 +956,4 @@ int main() {
 
     return 0;
 }
-
 
